fe6dc4387e1394297d4f1bfdf8e38b4d1c7b09ad
lan <lanyuyang@gmail.com>
2024-05-19 01:49:40 +0900

    fix claude agent

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tinyagent",
-    version="0.1.2",
+    version="0.1.3",
     author="y-lan",
     author_email="lanyuyang@gmail.com",
     description="A minimalistic agent framework",
